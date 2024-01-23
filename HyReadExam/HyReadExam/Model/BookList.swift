21170f14c69606910d5f1bc300eb05bcf5c7bafa
Mike <boxingbat@gmail.com>
2024-01-23 21:44:06 +0800

    set up HTTP Manager and BookListModel

---
+++ HyReadExam/HyReadExam/Model/BookList.swift
@@ -0,0 +1,17 @@
+//
+//  BookList.swift
+//  HyReadExam
+//
+//  Created by 1 on 2024/1/23.
+//
+
+import Foundation
+
+struct Book {
+    let uuid: Int
+    let title: String
+    let coverUrl: String
+    let publishDate: String
+    let publisher: String
+    let author: String
+}
