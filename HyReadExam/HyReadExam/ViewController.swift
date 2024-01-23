c16bd80f86b0c91c164c5cc7bd2e2f3782eddf63
Mike <boxingbat@gmail.com>
2024-01-23 20:45:41 +0800

    set up project

---
+++ HyReadExam/HyReadExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HyReadExam
+//
+//  Created by 1 on 2024/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
