1f4c2f1e0278de66c9fe47a02463a65b39aace29
Mike <boxingbat@gmail.com>
2024-01-24 20:39:06 +0800

    setup favorite button

--- HyReadExam/HyReadExam/View/BookCollectionViewCell.swift
+++ HyReadExam/HyReadExam/View/BookCollectionViewCell.swift
@@ -12,10 +12,15 @@ class BookCollectionViewCell: UICollectionViewCell {
 
     private let imageView = UIImageView()
     private let titleLabel = UILabel()
+    private let favoriteButton = UIButton()
+
+    //use closure to send back the event
+    var onFavoriteToggle: ((Int) -> Void)?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupCell()
+        setupFavoriteButton()
     }
 
     required init?(coder: NSCoder) {
@@ -53,12 +58,35 @@ class BookCollectionViewCell: UICollectionViewCell {
             titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
         ])
         let font = UIFont.systemFont(ofSize: UIFont.systemFontSize)
         let lineHeight = font.lineHeight
     }
+    private func setupFavoriteButton() {
+            favoriteButton.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview(favoriteButton)
 
+            NSLayoutConstraint.activate([
+                favoriteButton.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 8),
+                favoriteButton.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -8),
+                favoriteButton.widthAnchor.constraint(equalToConstant: 28),
+                favoriteButton.heightAnchor.constraint(equalToConstant: 28)
+            ])
+
+            favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            favoriteButton.tintColor = .white
+            favoriteButton.addTarget(self, action: #selector(toggleFavorite), for: .touchUpInside)
+        }
+    @objc private func toggleFavorite() {
+            if favoriteButton.tintColor == .white {
+                favoriteButton.tintColor = UIColor(red: 80/255, green: 227/255, blue: 194/255, alpha: 1) // #50E3C2
+                favoriteButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            } else {
+                favoriteButton.tintColor = .white
+                favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        }
     func configure(with book: Book) {
         titleLabel.text = book.title
         if let url = URL(string: "\(book.coverUrl)") {
